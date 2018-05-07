470652dc5fe82e3f1ab9a3f2e6e8fcc4849031d1
bittersweet95 <woraluk95@gmail.com>
2018-05-07 15:45:06 +0700

    สร้าง layout fusion chart

---
+++ application/views/fishing-vessel/report.php
@@ -0,0 +1,12 @@
+<div class"row">
+    <div class="col-6">
+        <div class="card">
+            <div class="card-header">
+                จำนวนเรือแยกตามประเภท
+</div>
+<div class="card-body">
+<div id
+</div>
+<div class="col-6">
+</div>
+</div>
