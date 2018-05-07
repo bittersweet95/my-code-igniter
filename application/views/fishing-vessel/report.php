e2d20bfde486e41ed7417db4b6400b7f226c1db8
bittersweet95 <woraluk95@gmail.com>
2018-05-07 16:12:08 +0700

    สร้างหน้าแสดงข้อมูลเรือประมงตามประเทศเป็น bar chart

--- application/views/fishing-vessel/report.php
+++ application/views/fishing-vessel/report.php
@@ -1,12 +1,13 @@
+<script src="<?php=base_url() ?>assets/js/mychart.js"></script>
 <div class"row">
     <div class="col-6">
         <div class="card">
             <div class="card-header">
                 จำนวนเรือแยกตามประเภท
-</div>
-<div class="card-body">
-<div id
-</div>
-<div class="col-6">
-</div>
+            </div>
+        <div class="card-body">
+            <div id="bar-chart">
+
+            </div>
+    </div>
 </div>
