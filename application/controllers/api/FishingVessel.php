dcf7d924e5120c95c9dfe8d09c1cec47e14301ed
bittersweet95 <woraluk95@gmail.com>
2018-05-10 14:40:04 +0700

    สร้างฟังก์ชันรับโพสต์เป็น json จากโมบายแอป บันทีกลง ดาต้าเบส และส่งค่า true/false กลับไปให้โมบายแอป

--- application/controllers/api/FishingVessel.php
+++ application/controllers/api/FishingVessel.php
@@ -15,9 +15,18 @@ class FishingVessel extends CI_Controller {
         //var_dump($data);
         echo json_encode($data['vessels']);
     }
+    /// /api/fishingvessel/create
     public function create()
     {
-        
+        $input_data = json_decode(trim(file_get_contents('php://input')),true);
+        $vesselName = $input_data['Name'];
+        $countryID = $input_data['Country_ID'];
+
+        $this->load->model('fishingvessel_model');
+        $result = $this->fishingvessel_model->save_new_vessel_with_variable($vesselName,$countryID);
+
+        $data['result_info'] = $result;
+        echo json_encode($data);
     }
 
 }
